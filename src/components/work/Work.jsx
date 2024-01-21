9fc14d374190426106b17a1f983d8fe6c63fab99
= <=>
2024-01-21 10:01:32 -0500

    feat: add works translation

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -1,14 +1,17 @@
 import Works from './Works';
 import "../../styles/work/Work.css";
+import { useTranslation } from "react-i18next";
 
 const Work = () => {
+
+  const { t } = useTranslation("global");
   
   return (
     <section className="work section" id='portfolio'>
-      <span className="section__subtitle"></span>
-        <h2 className="section__title"></h2>
+      <h2 className="section__title">{t("work.work__subtitle")}</h2>
+      <span className="section__subtitle">{t("work.work__title")}</span>
         
-        <Works />
+      <Works />
     </section>
   )
 }
